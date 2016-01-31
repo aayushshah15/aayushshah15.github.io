7a30f083488d368898e5499320d28a5be0700de9
Aayush Shah <aayush.shah15@gmail.com>
2016-01-31 00:50:52 -0500

    Added Bootstrap.css and animate.css (WIP)

---
+++ app.js
@@ -0,0 +1,14 @@
+/*global angular */
+
+var mainApp = angular.module('mainApp', []);
+
+mainApp.controller('mainController', function ($scope, $log, $routeProvider) {
+
+    $scope.$apply(function () {
+
+        
+    
+    });
+    
+        
+});
