9f7928e94a6bfd919cec4a97ded8f0271a47aad9
Aayush Shah <aayush.shah15@gmail.com>
2016-02-04 20:06:28 -0500

    Added AngularJS

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 var mainApp = angular.module('mainApp', []);
 
-mainApp.controller('mainController', function ($scope, $log, $routeProvider) {
 
-    $scope.$apply(function () {
-
-        
+mainApp.controller('mainController', function ($scope, $log) {
+    "use strict";
+    $scope.showDiv = false;
     
-    });
-    
+    $scope.resumeShow = function () {
+        $scope.showDiv = !$scope.showDiv;
+    };
         
 });
