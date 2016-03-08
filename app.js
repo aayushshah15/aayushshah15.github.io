66eb79d24a1266b8554eef937cb0f6dc1ad6303f
Aayush Shah <aayush.shah15@gmail.com>
2016-03-08 15:17:58 -0500

    added autoscroll to the bottom

--- app.js
+++ app.js
@@ -3,12 +3,16 @@
 var mainApp = angular.module('mainApp', []);
 
 
-mainApp.controller('mainController', function ($scope, $log) {
+mainApp.controller('mainController', function ($scope, $log, $anchorScroll, $location) {
     "use strict";
     $scope.showDiv = false;
     
     $scope.resumeShow = function () {
         $scope.showDiv = !$scope.showDiv;
+    };
+    
+    $scope.scrollDown = function () {
+        $location.hash('bottom');
     };
         
 });
